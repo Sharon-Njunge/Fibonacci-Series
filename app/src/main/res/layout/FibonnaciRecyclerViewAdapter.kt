5669d5822b6f392b55979bf2c5330b89ffa0d69d
Sharon-Njung'e <njungesharonwanjiku@gmail.com>
2024-06-20 20:34:56 +0300

    add files

---
+++ app/src/main/res/layout/FibonnaciRecyclerViewAdapter.kt
@@ -0,0 +1,27 @@
+package layout
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+:
+class FibonnaciRecyclerViewAdapter (var numbers:List<Int>):RecyclerView.Adapter<NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val numberView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.numbers_list_items,parent,false)
+        return NumbersViewHolder(numberView)
+    }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumbers.text = numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+}
+
+class NumbersViewHolder(numberView: View):RecyclerView.ViewHolder(numberView){
+    val tvNumbers = numberView.findViewById<TextView>(R.id.tvNumbers)
+}
